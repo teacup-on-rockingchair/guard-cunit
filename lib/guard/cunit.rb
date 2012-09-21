4372dfab33a65fba622685d3e8ca09eff09948ba
root <root@terminus.batmbg.com>
2012-09-21 18:29:49 +0300

    Moved runner behaviour in a separate class and fixed Guardfile format

--- lib/guard/cunit.rb
+++ lib/guard/cunit.rb
@@ -1,78 +1,33 @@
 require 'guard'
 require 'guard/guard'
-require "guard/cunit/version"
 
 
 module Guard
   class Cunit < Guard 
-    @@cunit_runner=''
-    @@project_builder=''
-    @@project_cleaner=''
-    @@project_libdir=''
+    autoload :Runner,    'guard/cunit/runner'
+
     def initialize(watchers = [], options = {})
       super
       @options = {
         :all_on_start   => true,
       }.update(options)
+      @runner    = Runner.new(@options)
     end
-    
-    # set the executable file name to run CUNIT tests
-    def Cunit.set_runner(name)
-      @@cunit_runner=name
-    end
-    
-    # set command to run to prepare build
-    def Cunit.set_builder(name)
-      @@project_builder=name
-    end
-
-    # set cleaner script/exe/command
-    def Cunit.set_cleaner(name)
-      @@project_cleaner=name
-    end
-
-
-    # set directory where library under test is generated
-    def Cunit.set_libdir(name)
-      @@project_libdir=name
-    end
-    #
-    # run one phase of the guard via a system command/executable
-    #
-    def run_task(task_executable)
-      output = String.new
-      IO.popen(task_executable)  do |myio|
-        output = myio.read
-      end
-      throw :task_has_failed unless $?.exitstatus == 0
-#      puts output
-    end
-
-    # run clean before each run all start with clean
-    def run_clean
-      run_task(@@project_cleaner)
-    end
-    
-    # run unit tests via cunit executable
-    def run_tests
-      # setup environment so it should include lib dir for ld path
-      ENV["LD_LIBRARY_PATH"]="#{ENV["LD_LIBRARY_PATH"]}:#{@@project_libdir}"
-      run_task(@@cunit_runner)
-    end
-    
-    
-    # run make command to build the project
-    def run_make
-      run_task(@@project_builder)
-    end
-    
+#    def start
+#      super
+#      UI.info "Guard::Cunit is running!"
+#      run_all if @options[:all_on_start]
+#    end    
     # Called when just `enter` is pressed
     # This method should be principally used for long action like running all specs/tests/...
     # @raise [:task_has_failed] when run_all has failed
     def run_all
-      run_clean
-      run_make
-      run_tests
+      puts "Mine run all"
+      passed = @runner.run
+      throw :task_has_failed unless passed
+    end
+    def run_on_change
+      passed = run_all
     end
   end
 
@@ -87,24 +42,27 @@ module Guard
 
     # dsl call to set cunit test executable
     def cunit_runner (name)
-      Cunit.set_runner(name)
+      Cunit::Runner.set_runner(name)
     end
     
     # dsl call to set cunit build command/script, by default make
     def set_builder (name)
-      Cunit.set_builder(name)
+      Cunit::Runner.set_builder(name)
     end
     
     #dsl call to set cunit clean command/script, by default 'make clean'
     def set_cleaner (name)
-      Cunit.set_cleaner(name)
+      Cunit::Runner.set_cleaner(name)
     end
     
     # dsl call to set dir, where library under test is generated, by default current dir
     def libdir(name)
-      Cunit.set_libdir(File.absolute_path(name))
+      Cunit::Runner.set_libdir(File.absolute_path(name))
     end
 
+    def prydnya
+      UI.warning ("mama ti da eba bugliva")
+    end
   end
   
 end
