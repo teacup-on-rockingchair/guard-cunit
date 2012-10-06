06b4cd83ac895713a78754783b37c5e0f74e06a9
yggeorgiev <yavor.g.georgiev@gmail.com>
2012-10-06 21:58:26 +0300

    fix the test

--- spec/guard_cunit_spec.rb
+++ spec/guard_cunit_spec.rb
@@ -37,14 +37,10 @@ describe Guard::Cunit do
 
     it "should run build on changes " do
       
-      guardfile_has_unit_test_exe()
-      popen_successfull_fake("make clean")
-      popen_successfull_fake("make 2>&1")
-      fake_test_exe("#{File.basename(Dir.getwd)}_unit",:pass)
       cguard = Guard::Cunit.new
       cguard.stub(:run_all).and_return(true)
       Guard::UI.should_receive(:info).with("Process changes in #{File.basename(Dir.getwd)}")
-      Guard::setup
+      
       cguard.run_on_change("#{File.basename(Dir.getwd)}")
       
     end
