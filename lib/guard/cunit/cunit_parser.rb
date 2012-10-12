b1bdec45bf9ce9dde9e9ef1a067e3fec4461960d
strandjata <strandjata@gmail.com>
2012-10-12 07:55:37 +0300

    add parser tests

--- lib/guard/cunit/cunit_parser.rb
+++ lib/guard/cunit/cunit_parser.rb
@@ -1,6 +1,19 @@
 module Guard
   class Cunit
    class CunitParser
+     @output
+     def initialize (task_output)
+       parse_output( task_output )
+     end
+
+     def parse_output( task_output )
+       @output = task_output
+     end
+
+     def full_output
+       @output
+     end
+
    end
  end
 end
